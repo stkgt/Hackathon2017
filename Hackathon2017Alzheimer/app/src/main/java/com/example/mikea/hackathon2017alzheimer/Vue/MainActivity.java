922e1d6656b037177d1b8a3a8b48b3709b5addc1
Unknown <mike.aubenas@gmail.com>
2017-11-09 01:14:03 +0100

    Mise en place de packages

--- Hackathon2017Alzheimer/app/src/main/java/com/example/mikea/hackathon2017alzheimer/Vue/MainActivity.java
+++ Hackathon2017Alzheimer/app/src/main/java/com/example/mikea/hackathon2017alzheimer/Vue/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.mikea.hackathon2017alzheimer;
+package com.example.mikea.hackathon2017alzheimer.Vue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.mikea.hackathon2017alzheimer.R;
 
 public class MainActivity extends AppCompatActivity
 {
